c924a02969813b3df71b041011d39bf1641c2b25
이재윤B/SGS SEED개발팀 <jaeylee@smilegate.com>
2022-03-25 21:04:30 +0900

    add ts object type

--- class-note/1_type_basic.ts
+++ class-note/1_type_basic.ts
@@ -11,3 +11,16 @@ let num: number = 10;
 let arr: Array<number> = [1, 2, 3]
 let heros: Array<string> = ['Captain', 'Thor', 'Hulk']
 let items: number[] = [1, 2, 3]
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 1]
+
+// TS 객체
+let obj: object = {}
+let person: {name: string, age: number} = {
+    name: 'capt',
+    age: 100
+}
+
+// TS 진위값
+let show: boolean = true
