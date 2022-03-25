c47feab64f688fd1764d00f645bbbcbabf85404a
이재윤B/SGS SEED개발팀 <jaeylee@smilegate.com>
2022-03-25 20:13:04 +0900

    apply jsdoc

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,5 +1,17 @@
+// function sum(a, b) {
+//     return a + b
+// }
+
+// var result = sum(10, 20)
+
+// @ts-check
+
+/**
+ * @param {number} a 첫 번째 숫자
+ * @param {number} b 두 번째 숫자
+ */
 function sum(a, b) {
     return a + b
 }
 
-var result = sum(10, 20)
+sum(10, '20')
