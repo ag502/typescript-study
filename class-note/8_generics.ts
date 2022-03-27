65d6a38534758aa79a9dbaecf9e83a1768020181
ag502 <ag502@gmail.com>
2022-03-27 15:46:45 +0900

    add ts generics

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -14,17 +14,34 @@
 
 // logText<string>("하이");
 
-function logText(text: string) {
+// function logText(text: string) {
+//   console.log(text);
+//   //   text.split("").reverse().join("");
+//   return text;
+// }
+
+// function logNumber(num: number) {
+//   console.log(num);
+//   return num;
+// }
+
+// logText("a");
+// logNumber(10);
+
+// function logText(text: string | number) {
+//   console.log(text);
+//   return text;
+// }
+
+// const a = logText("a");
+// logText(10);
+
+function logText<T>(text: T): T {
   console.log(text);
-  //   text.split("").reverse().join("");
   return text;
 }
 
-function logNumber(num: number) {
-  console.log(num);
-  return num;
-}
+const str = logText<string>("a");
+str.split("");
 
-logText("a");
-logNumber(10);
-logText(true);
+const login = logText<boolean>(true);
