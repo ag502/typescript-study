32a4bb54e1f33508875182326eac73f37185abe6
ag502 <ag502@naver.com>
2022-04-24 15:57:54 +0900

    basics-object-types

---
+++ review/6.basics-object-types/app.js
@@ -0,0 +1,15 @@
+var person = {
+    name: "lee",
+    age: 26
+};
+console.log(person);
+var product = {
+    id: "abc1",
+    price: 12.99,
+    tags: ["great-offer", "hot-and-new"],
+    details: {
+        title: "Red Carpet",
+        description: "A great carpet - almost brand-new"
+    }
+};
+console.log(product.details);
