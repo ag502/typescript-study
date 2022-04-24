2cafb0a9f01f7c877bd87445fbb2ec15efdf2dd0
ag502 <ag502@naver.com>
2022-04-24 17:41:10 +0900

    basics-type-aliases

--- review/11.basics-type-aliases/app.ts
+++ review/11.basics-type-aliases/app.ts
@@ -6,3 +6,16 @@ function combine(
   input2: Combinable,
   resultConversion: CoversionDescriptor
 ) {}
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const u1: User = { name: "Max", age: 26 };
+
+function greet(user: { name: string; age: number }) {}
+function isOlder(user: { name: string; age: number }) {}
+
+function greet2(user: User) {}
+function isOlder2(user: User) {}
