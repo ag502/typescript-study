24e7472ce1d8b8e66176a46c8d799540508d1bbb
ag502 <ag502@naver.com>
2022-04-24 16:34:26 +0900

    basics-union-types

---
+++ review/10.basics-union-types/app.js
@@ -0,0 +1,8 @@
+function combine(input1, input2) {
+    var result = input1 + input2;
+    return result;
+}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combineNames = combine("Max", "Anna");
+console.log(combinedAges);
