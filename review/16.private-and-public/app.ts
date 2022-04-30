3585ebf7c4917b3350fe4be2c10fe7ca929d445d
ag502 <ag502@naver.com>
2022-04-30 12:40:23 +0900

    private-and-public

---
+++ review/16.private-and-public/app.ts
@@ -0,0 +1,32 @@
+class Department {
+  public name: string;
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.name = "Lee";
+accounting.employees[2] = "Anna"; // unexpected access
+
+accounting.describe();
+accounting.printEmployeeInformation();
