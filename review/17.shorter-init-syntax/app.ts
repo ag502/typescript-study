87703f1fcc6d8c410a84dfcd3cba623df5bc507e
ag502 <ag502@naver.com>
2022-05-01 22:20:15 +0900

    shorter-init-syntax

---
+++ review/17.shorter-init-syntax/app.ts
@@ -0,0 +1,14 @@
+class Department {
+  //   private id: string;
+  //   private name: string;
+  //   private employees: string[];
+
+  constructor(
+    private id: string,
+    private name: string,
+    private employees: string[]
+  ) {
+    this.id = id;
+    this.name = name;
+  }
+}
