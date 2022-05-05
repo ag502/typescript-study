0d4a5604dde79fb69a76bf0b7580a70aed5e978e
ag502 <ag502@naver.com>
2022-05-05 16:57:58 +0900

    abstract

---
+++ review/23.abstract/app.ts
@@ -0,0 +1,91 @@
+abstract class Department {
+  static fiscalYear = 2020;
+  protected employees: string[] = [];
+
+  constructor(protected readonly id: string, public name: string) {}
+
+  static createEmployee(name: string) {
+    return { name };
+  }
+
+  abstract describe(this: Department): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+
+  describe(): void {
+    console.log("IT Department - ID: " + this.id);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No Report Found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please Pass Value");
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  describe() {
+    console.log(`Accounting Department - ID: ${this.id}`);
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", []);
+it.describe();
+
+const accounting = new AccountingDepartment("d2", []);
+
+accounting.mostRecentReport = "Year End Report";
+accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.describe();
