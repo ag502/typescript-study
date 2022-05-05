3015ee38c6318d04206555bfff7402b6ac8179f5
ag502 <ag502@naver.com>
2022-05-06 00:48:30 +0900

    generics-utility-type

---
+++ review/43.generics-utility-types/app.ts
@@ -0,0 +1,23 @@
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  //   return { title, description, completeUntil: date };
+  let courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Sports"];
+// names.push("Manu"); error
