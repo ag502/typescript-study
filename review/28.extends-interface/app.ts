99a34f66a73bf2c3bebf835b04263e1339ec56ae
ag502 <ag502@naver.com>
2022-05-05 18:22:19 +0900

    extends-interface

---
+++ review/28.extends-interface/app.ts
@@ -0,0 +1,25 @@
+interface Named {
+  readonly name: string;
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}`);
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Max");
+
+user1.greet("HI there I am");
