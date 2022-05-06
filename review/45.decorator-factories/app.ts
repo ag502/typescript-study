dd894fd1d857df7be35930bcc0f14d31dfecbf15
ag502 <ag502@naver.com>
2022-05-06 23:17:40 +0900

    decorator-factories

---
+++ review/45.decorator-factories/app.ts
@@ -0,0 +1,15 @@
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+@Logger("LOGGING - PERSON")
+class Person {
+  name = "Max";
+
+  constructor() {
+    console.log("Creating person object...");
+  }
+}
