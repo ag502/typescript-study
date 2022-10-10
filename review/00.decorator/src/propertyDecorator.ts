533297cfe476efc27e4e349ca4d6739860382ad4
ag502 <ag502@naver.com>
2022-10-10 21:10:56 +0900

    add: parameter decorator

---
+++ review/00.decorator/src/propertyDecorator.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+
+const formatMetadataKey = Symbol("format");
+
+function format(formatString: string) {
+  return Reflect.metadata(formatMetadataKey, formatString);
+}
+
+function getFormat(target: any, propertyKey: string) {
+  return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
+}
+
+class Formatter {
+  @format("Hello, %s")
+  greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  greet() {
+    let formatString = getFormat(this, "greeting");
+    return formatString.replace("%s", this.greeting);
+  }
+}
+
+console.log(new Formatter("test").greet());
